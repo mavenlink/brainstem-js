4760096465e60a981c98d4fc8753bb9504b68ef9
Jamie Rolfs <jamie.rolfs@gmail.com>
2016-03-24 21:16:19 -0700

    Re-add gem library structure

---
+++ lib/brainstem/js/engine.rb
@@ -0,0 +1,5 @@
+module Brainstemjs
+  class Rails < ::Rails::Engine
+    # So Rails will add vendor to the asset pipeline
+  end 
+end
---
+++ lib/brainstem/js/version.rb
@@ -0,0 +1,5 @@
+module Brainstem
+  module Js
+    VERSION = "0.3.1"
+  end
+end
