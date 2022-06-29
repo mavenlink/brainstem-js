c3f9a498510259e746840380b0066c429d68f11e
Justin Hurstwright <jhurstwright@mavenlink.com>
2022-06-29 10:50:28 -0500

    ref: Remove all references to coffee

--- gulpfile.js
+++ gulpfile.js
@@ -6,56 +6,31 @@ const replace = require('gulp-replace');
 const download = require('gulp-download');
 const path = require('path');
 const del = require('del');
-const coffee = require('gulp-coffee');
 const stream = require('vinyl-source-stream');
 const browserify = require('browserify');
-const coffeeify = require('coffeeify');
 const shim = require('browserify-shim');
-const coffeelint = require('gulp-coffeelint');
 const { Server: Karma } = require('karma');
 
 const { version, standalone, filename } = require('./package');
 
 
-const source = './src/**/*.coffee';
+const source = './src/**/*.js';
 const options = minimist(process.argv.slice(2));
 
 const moduleOutput = './lib';
-
-const styleguide = 'https://raw.githubusercontent.com/mavenlink/coffeescript-style-guide/master';
-const lintConfig = 'coffeelint.json';
 
 
 // Tasks
 
 gulp.task('build-module', () => {
   return gulp.src(source)
-    .pipe(coffee())
     .pipe(gulp.dest(moduleOutput))
     .on('error', util.log);
 });
 
-gulp.task('fetch-styleguide', () => {
-  const url = `${styleguide}/${lintConfig}`;
-
-  return download(url).pipe(gulp.dest('.'));
-});
-
-gulp.task('coffeelint', gulp.series('fetch-styleguide', () => {
-  return gulp.src(source)
-    .pipe(coffeelint())
-    .pipe(coffeelint.reporter('coffeelint-stylish'))
-    .pipe(coffeelint.reporter('failOnWarning'));
-}));
-
 gulp.task('clean-module', () => {
   return del(`${moduleOutput}/**/*.js`);
 });
-
-gulp.task('clean-styleguide', () => {
-  return del(lintConfig);
-});
-
 
 let karmaConfig = {
   configFile: path.join(__dirname, 'karma.conf.js'),
@@ -109,4 +84,4 @@ gulp.task('test-debug', (done) => {
   new Karma(config, karmaErrorHandler.bind(done)).start();
 });
 
-gulp.task('ci', gulp.series(gulp.parallel(['coffeelint', 'test-ci'])));
+gulp.task('ci', gulp.series(gulp.parallel(['test-ci'])));
